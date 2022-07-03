d9cc04bfbd175d278994a44999ed1e4cdf2e67e3
Arugaf <arug113@gmail.com>
2022-07-03 18:19:08 +0300

    script: mob behaviour edited

--- Assets/Scripts/Enemies/Mob.cs
+++ Assets/Scripts/Enemies/Mob.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using Player;
 using UnityEngine;
 
 namespace Enemies {
@@ -9,20 +8,118 @@ namespace Enemies {
             Foe
         }
 
+        private Sanity _sanity;
+        [SerializeField] private float addToSanity = 50f;
+        [SerializeField] private float subtractFromSanity = 50f;
+
         [SerializeField] private EnemyType type;
+
+        [SerializeField] private float deleteMobDelay = 5f;
+
+        private GameObject _spawner;
+
+        private bool _active;
+
+        private Rigidbody2D _rigidbody;
+
+        private UnityEngine.Light _light;
+
+        private Vector2 _velocity = Vector2.zero;
+
+        private const float KVelocityMultiplier = 50f;
 
         // private Transform _transform;
 
-        public Mob(EnemyType type, Vector2 position) {
-            this.type = type;
+        public void SetType(EnemyType typeTo) {
+            type = typeTo;
+
+            var color = GetComponent<SpriteRenderer>();
+
+            switch (type) {
+                case EnemyType.Foe: {
+                    color.color = Color.red;
+                    break;
+                }
+
+                case EnemyType.Ally: {
+                    color.color = Color.cyan;
+                    break;
+                }
+            }
         }
 
-        // Start is called before the first frame update
+        private void OnDestroy() {
+            if (_spawner) {
+                _spawner.GetComponent<Spawner>().DeleteMob();
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player")) {
+                if (!_sanity) {
+                    return;
+                }
+
+                switch (type) {
+                    case EnemyType.Ally: {
+                        _sanity.AddSanity(addToSanity);
+                        break;
+                    }
+                    case EnemyType.Foe: {
+                        _sanity.SubtractSanity(subtractFromSanity);
+                        break;
+                    }
+                }
+
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D other) {
+            if (other.CompareTag("LightSource")) {
+                _active = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other) {
+            if (other.CompareTag("LightSource")) {
+                _active = false;
+            }
+        }
+
+        private void DeleteIfInactive() {
+            if (!_active) {
+                Destroy(gameObject);
+            }
+        }
+
+        private void FixedUpdate() {
+            if (!_active) {
+                return;
+            }
+
+            var currentVelocity = _rigidbody.velocity;
+
+            var lightPos = _light.transform.position;
+            var pos = transform.position;
+
+            var velocity = (lightPos - pos) * (Time.fixedDeltaTime * KVelocityMultiplier * Mathf.InverseLerp(
+                0.1f,
+                1,
+                1 / Vector2.Distance(lightPos, pos)
+            ));
+
+            _rigidbody.velocity =
+                Vector2.SmoothDamp(currentVelocity, velocity, ref _velocity, 0.05f);
+        }
+
         void Start() {
-            // _transform = GetComponent<Transform>();
+            _spawner = GameObject.FindWithTag("Spawner");
+            _light = GameObject.FindWithTag("LightSource").GetComponent<UnityEngine.Light>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            var sanity = GameObject.FindWithTag("Sanity");
+            _sanity = sanity.GetComponent<Sanity>();
+            InvokeRepeating(nameof(DeleteIfInactive), deleteMobDelay, deleteMobDelay);
         }
-
-        // Update is called once per frame
-        void Update() { }
     }
 }
